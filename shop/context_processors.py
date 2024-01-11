b29080da5d45d8d1153f1c4e01a7b7571bc3ed91
arathigp <arathigp89@gmail.com>
2024-01-11 11:36:25 +0530

    ecom

---
+++ shop/context_processors.py
@@ -0,0 +1,5 @@
+#set all links used in this program
+from .models import Category
+def menu_links(request):
+    links=Category.objects.all()
+    return dict(links=links)
---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from .import views
+from django.urls import path
+app_name='shop'
+urlpatterns=[
+    path('',views.allprodCat,name='allprodCat'),
+    path('<slug:c_slug>/',views.allprodCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='proDetail'),
+
+
+
+
+]
